5a57ac0a3d8de375e279082d378699251c01c9f0
Jo Dickson <jogreybill@gmail.com>
2015-09-23 07:36:07 -0500

    Ran phptidy on custom-post-types.php; re-added person CPT and org_group taxonomy

--- custom-taxonomies.php
+++ custom-taxonomies.php
@@ -86,6 +86,30 @@ abstract class CustomTaxonomy {
 }
 
 
+/**
+ * Describes organizational groups
+ *
+ * @author Chris Conover
+ **/
+class OrganizationalGroups extends CustomTaxonomy {
+	public
+		$name               = 'org_groups',
+		$general_name       = 'Organizational Groups',
+		$singular_name      = 'Organizational Group',
+		$search_items       = 'Search Organizational Groups',
+		$popular_items      = 'Popular Organizational Groups',
+		$all_times          = 'All Organizational Groups',
+		$parent_item        = 'Parent Organizational Group',
+		$parent_item_colon  = 'Parent Organizational Group:',
+		$edit_item          = 'Edit Organizational Group',
+		$update_item        = 'Update Organizational Group',
+		$add_new_item       = 'Add New Organizational Group',
+		$new_item_name      = 'New Tag Organizational Group',
+
+		$hierarchical = True;
+}
+
+
 class ClusterLeads extends CustomTaxonomy {
 	public
 		$name               = 'cluster_leads',
